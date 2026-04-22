2a884b99f1e15d89fde3343a86384252f20a08b3
David Nascimento <dnmnascimento@outlook.com>
2026-04-21 23:09:32 -0300

    nicio de projeto, pós configuração e criação de Container docker, construção da estrutura de diretórios básicos do projeto.

---
+++ src/main/java/com/david/api_boavista/ApiBoavistaApplication.java
@@ -0,0 +1,13 @@
+package com.david.api_boavista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBoavistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBoavistaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/api_boavista/ApiBoavistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.api_boavista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBoavistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
