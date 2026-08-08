f6ff49e81b2ce574ece02e63139a50821870e3e5
David Nascimento <dnmnascimento@outlook.com>
2026-08-08 00:01:33 -0300

    Implementação JwtService e configuração da chave JWT.

---
+++ src/main/java/com/david/api_boavista/service/JwtService.java
@@ -0,0 +1,45 @@
+package com.david.api_boavista.service;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.david.api_boavista.entities.Usuario;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+    
+    private final SecretKey secretKey;
+    private final long expirationTime;
+
+    public JwtService(
+        @Value("${jwt.secret}") String secret,
+        @Value("${jwt.expiration-time}") long expiration) {
+
+            byte[] keyBytes = Decoders.BASE64.decode(secret);
+
+            this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+            this.expirationTime = expiration;
+    }
+
+    public String gerarToken(Usuario usuario) {
+
+        Date agora = new Date();
+        Date expiracao = new Date(agora.getTime() + expirationTime);
+
+        return Jwts.builder()
+            .subject(usuario.getUsername())
+            .claim("role", usuario.getRole().getAuthority())
+            .issuedAt(agora)
+            .expiration(expiracao)
+            .signWith(secretKey)
+            .compact();
+    }
+}
