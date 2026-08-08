ac36f35714479184bd812c6a7a19eb3c22c6b9e6
David Nascimento <dnmnascimento@outlook.com>
2026-08-07 23:15:08 -0300

    Construção do UserDEtailsServiceImpl para auxiliar o processo de login.

---
+++ src/main/java/com/david/api_boavista/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.david.api_boavista.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.david.api_boavista.entities.Usuario;
+import com.david.api_boavista.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    // Implement the methods of UserDetailsService here
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        Usuario usuario = usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return usuario;
+        
+    }
+}
