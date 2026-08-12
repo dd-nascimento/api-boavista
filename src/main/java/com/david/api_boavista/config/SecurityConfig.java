1743e6c67e97ad6853704c828ccdb4fd01e41411
David Nascimento <dnmnascimento@outlook.com>
2026-08-11 23:40:14 -0300

    implement authentication login with JWT.

--- src/main/java/com/david/api_boavista/config/SecurityConfig.java
+++ src/main/java/com/david/api_boavista/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.david.api_boavista.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +16,18 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+                .requestMatchers("/auth/login").permitAll()
+                .anyRequest().authenticated()
             );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+        AuthenticationConfiguration configuration) 
+        throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }
