0e1604f5e41482faf4cbe2a5bac352bb62358c1d
David Nascimento <dnmnascimento@outlook.com>
2026-09-23 22:16:04 -0300

    test: adiciona teste de sucesso para salvar usuário

--- src/test/java/com/david/api_boavista/service/UsuarioServiceTest.java
+++ src/test/java/com/david/api_boavista/service/UsuarioServiceTest.java
@@ -11,14 +11,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.mockito.ArgumentCaptor;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.david.api_boavista.dto.UsuarioRequestDTO;
 import com.david.api_boavista.dto.UsuarioResponseDTO;
 import com.david.api_boavista.entities.Usuario;
 import com.david.api_boavista.exception.UsuarioNaoEncontradoException;
 import com.david.api_boavista.repository.UsuarioRepository;
 
-import jakarta.inject.Inject;
 
 @ExtendWith(MockitoExtension.class)
 public class UsuarioServiceTest {
@@ -61,5 +66,47 @@ public class UsuarioServiceTest {
         assertThrows(UsuarioNaoEncontradoException.class, () -> {
             usuarioService.buscarPorId(1L);
         });
+    }
+
+    @Test
+    void salvar_deveRetornarUsuarioQuandoEmailNaoExiste() {
+
+        UsuarioRequestDTO usuarioRequestDTO = new UsuarioRequestDTO();
+        usuarioRequestDTO.setNome("João Henrique Moura");
+        usuarioRequestDTO.setEmail("jhmoura@apiboavista.com");
+        usuarioRequestDTO.setSenha("123456");
+
+        when(usuarioRepository.existsByEmail(usuarioRequestDTO.getEmail()))
+            .thenReturn(false);
+        
+        when(passwordEncoder.encode(usuarioRequestDTO.getSenha()))
+            .thenReturn("senhaCriptografada");
+        
+        Usuario usuarioSalvo = new Usuario();
+        usuarioSalvo.setId(1L);
+        usuarioSalvo.setNome("João Henrique Moura");
+        usuarioSalvo.setEmail("jhmoura@apiboavista.com");
+
+        when(usuarioRepository.save(any(Usuario.class)))
+            .thenReturn(usuarioSalvo);
+        
+        UsuarioResponseDTO resultado =
+            usuarioService.salvar(usuarioRequestDTO);
+        
+        assertEquals(1L, resultado.getId());
+        assertEquals("João Henrique Moura", resultado.getNome());
+        assertEquals("jhmoura@apiboavista.com", resultado.getEmail());
+        verify(passwordEncoder).encode(usuarioRequestDTO.getSenha());
+
+        ArgumentCaptor<Usuario> usuarioCaptor =
+            ArgumentCaptor.forClass(Usuario.class);
+
+        verify(usuarioRepository).save(usuarioCaptor.capture());
+
+        Usuario usuarioSalvoCapturado = usuarioCaptor.getValue();
+        
+        assertEquals("senhaCriptografada", usuarioSalvoCapturado.getSenha());
+        assertEquals("João Henrique Moura", usuarioSalvoCapturado.getNome());
+        assertEquals("jhmoura@apiboavista.com", usuarioSalvoCapturado.getEmail());
     }
 }
